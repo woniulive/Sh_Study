0d90511dcaa4d47df9ca3d564867ed48068a6388
zjq <hiwszjq@126.com>
2019-02-14 16:57:30 +0800

    show how to display " in shell

---
+++ ec_char.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#how to display Hello "World"?
+#like below?
+echo "Hello "World""
+#"Hello "World"" is "Hello "+world+"", so actually it is one parameter
+#and it will print Hello World. you can test it by type "ls "Hello "World"""
+#below should be correct
+echo "Hello \"World\""
+
