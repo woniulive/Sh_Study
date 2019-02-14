22ca58c6a801cb0b54de9f04535e97ea5bc04f38
zjq <hiwszjq@126.com>
2019-02-14 14:12:39 +0800

    add read command

--- hello.sh
+++ hello.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "hello $USER"
-echo "hey i am "$USER" and will"
-echo "running process List"
+echo "what is your first name?";
+read a;
+echo "welcome Mr./Mrs. $a,would you like to tell us,what is your last name?";
+read b;
+echo "Thanks Mr./Mrs. $a $b for telling us your name";
+echo "***************"
+echo "Mr./Mrs. $b, it is time to say good bye"
 ps
 
