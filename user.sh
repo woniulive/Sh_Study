f0fc30131d30fe3efcb57e66a97b4389ac35531e
zjq <hiwszjq@126.com>
2019-02-14 16:33:56 +0800

    show how to refer to variable and avoid string separate

--- user.sh
+++ user.sh
@@ -2,5 +2,7 @@
 echo "what is your name?"
 read USER_NAME
 echo "Hello $USER_NAME"
-echo "I will create you a file called $USER_NAME_file"
-touch $USER_NAME_file
+echo "I will create you a file called ${USER_NAME}_file"
+#if you don't use double quotes below
+#then you enter Steve Parker, it will create two fils named Steve and Parker_file
+touch "${USER_NAME}_file"
