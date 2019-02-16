585e527db72bfa12c1a3d1088cfbec83a2f95318
zjq <hiwszjq@126.com>
2019-02-16 14:02:22 +0800

    add example to show

---
+++ var3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#bash var3.sh hello world earth
+echo "I was called with $# parameters"
+echo "My name is $0"
+echo "My name is `basename $0`"
+echo "My first parameter is $1"
+echo "My second parameter is $2"
+echo "All parameters are $@"
+
