072ab486727082cbd7d3da3222be47ed17cd865e
zjq <hiwszjq@126.com>
2019-02-18 14:40:14 +0800

    add example to show variable state if use |

--- function2.sh
+++ function2.sh
@@ -11,6 +11,9 @@ myfunc()
 echo "Script was called with $@"
 x=1
 echo "x is $x"
+#x will be 1,since tee start first and create a new shell
+myfunc 1 3 5 | tee out.log
+#x will be 2
 myfunc 1 3 5
 echo "x is $x"
 
