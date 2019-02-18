8a4a37e588bf6e011a17f77e85e9944bf04a82d5
zjq <hiwszjq@126.com>
2019-02-18 10:39:32 +0800

    add example to show $*

--- var4.sh
+++ var4.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#run this shell with command below
+#bash var4.sh var1 var2 var3 var4
 while [ "$#" -gt "0" ]
 do
     echo "\$1 is $1"
 #please refer to other doc to know about shift
     shift
 done
+echo "=============="
+echo "***run hello.sh***"
+bash hello.sh
+echo "shell run finshed"
+echo "***judge result***"
+#$? contains the exit value of last run command
+if [ "$?" -ne "0" ];then
+    echo "Sorry, we have problem here"
+else
+    echo "last command run successful"
+fi
 
