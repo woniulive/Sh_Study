987eb6ae352cccf949ead231a239b21ca4f5311f
zjq <hiwszjq@126.com>
2019-02-18 14:49:14 +0800

    show varibles scope again

---
+++ function3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+myfunc()
+{
+    echo "\$1 is $1"
+    echo "\$2 is $2"
+    #we cannot change $1 - we'd have to say:
+    #1="Goodbye Cruel"
+    #which is not a valid syntax. However, we can
+    #change $a:
+    a="Goodbye Cruel"
+}
+
+### Main script starts here
+
+a=hello
+b=world
+myfunc $a $b
+echo "a is $a"
+echo "b is $b"
+
